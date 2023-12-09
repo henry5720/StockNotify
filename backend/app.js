061cda6e865dec54cf4f6e7bda2eecd8e3e8b3b1
henry <henry@hotmail.com.tw>
2023-12-09 21:57:37 +0800

    [Init] backend: express + swagger + eslint + prettier + docker

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { resolve, dirname } from "path";
+import express, { json } from "express";
+import { serve, setup } from "swagger-ui-express";
+import dotenv from "dotenv";
+import test from "./routes/test.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const JsonPath = resolve(__dirname, "./swagger.json");
+const jsonContent = readFileSync(JsonPath, "utf-8");
+const swaggerDocument = JSON.parse(jsonContent);
+
+dotenv.config();
+const port = process.env.PORT;
+const app = express();
+app.use(json()); // 中間件解析 JSON 請求主體
+app.use("/swagger", serve, setup(swaggerDocument)); // 設置 Swagger UI
+app.get('/', (req, res) => {
+    // res.redirect('/swagger');
+    res.send('<h1>Welcome!</h1>');
+});
+app.use("/api", test);
+
+app.listen(port, () => console.log(`[server] http://localhost:${port}`));
---
+++ backend/routes/test.js
@@ -0,0 +1,16 @@
+// routes/users.js
+import { Router } from "express";
+const test = Router();
+
+// GET /test
+test.get("/test", (req, res) => {
+    res.json({ message: "[get] test sucess" });
+});
+
+// POST /test
+test.post("/test", (req, res) => {
+    const { test } = req.body;
+    res.json({ message: `[post] test sucess : ${test}` });
+});
+
+export default test;
